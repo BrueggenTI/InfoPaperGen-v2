bcea4c234ced553f448ef7c7df75e53b4c25c4e6
BrueggenTI <de_pu_digi_1@brueggen.com>
2025-08-22 10:08:33 +0200

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
-import { setupVite, serveStatic, log } from "./vite";
+import { serveStatic, log } from "./vite";
 import { azurePerformanceMiddleware } from "./middleware/azure-monitoring";
 
 const app = express();
@@ -82,6 +82,7 @@ app.use((req, res, next) => {
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
   if (app.get("env") === "development") {
+    const { setupVite } = await import("./vite.js");
     await setupVite(app, server);
   } else {
     serveStatic(app);
