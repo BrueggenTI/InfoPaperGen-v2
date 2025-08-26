21f35bf0ba0a04eabb22f4c4e3613fe1bf117cbd
DEPUDIGI1 <44242356-DEPUDIGI1@users.noreply.replit.com>
2025-08-26 08:05:16 +0000

    Restored to '970e165ae75930f2e62cd6048ac8f68308dc878d'
    Replit-Restored-To: 970e165ae75930f2e62cd6048ac8f68308dc878d

--- server/vite.ts
+++ server/vite.ts
@@ -6,7 +6,7 @@ import path from "path";
 import { createServer as createViteServer, createLogger } from "vite";
 import { type Server } from "http";
 import viteConfig from "../vite.config";
-
+import { nanoid } from "nanoid";
 
 // Das Vite-spezifische Logging bleibt hier.
 const viteLogger = createLogger();
@@ -42,7 +42,11 @@ export async function setupVite(app: Express, server: Server) {
         "client",
         "index.html",
       );
-      const template = await fs.promises.readFile(clientTemplate, "utf-8");
+      let template = await fs.promises.readFile(clientTemplate, "utf-8");
+      template = template.replace(
+        `src="/src/main.tsx"`,
+        `src="/src/main.tsx?v=${nanoid()}`,
+      );
       const page = await vite.transformIndexHtml(url, template);
       res.status(200).set({ "Content-Type": "text/html" }).end(page);
     } catch (e) {
